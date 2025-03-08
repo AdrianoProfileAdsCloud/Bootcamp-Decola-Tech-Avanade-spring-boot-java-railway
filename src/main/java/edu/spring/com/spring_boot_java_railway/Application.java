1f3edec9a5d163a5b07c7adc603b37a8661d623e
Adriano Aparecido da Silva <adriano.profile.ads.cloud@gmail.com>
2025-03-08 01:11:50 -0300

    Initial commit

---
+++ src/main/java/edu/spring/com/spring_boot_java_railway/Application.java
@@ -0,0 +1,13 @@
+package edu.spring.com.spring_boot_java_railway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/spring/com/spring_boot_java_railway/SpringBootJavaRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.spring.com.spring_boot_java_railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJavaRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
